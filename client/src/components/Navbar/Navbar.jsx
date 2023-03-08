9b5e595d3e3c4cd02f31f1c958c5eabc53f46a16
devgenx <sebastian110420@gmail.com>
2023-03-08 12:15:25 -0500

    Added logout dropdown

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,9 +2,13 @@ import "./navbar.scss";
 import styled from "styled-components";
 import Logo from "../Logo/Logo";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import defaultUser from "../../assets/default-user.png";
+import { FaUserCircle, FaCaretDown } from "react-icons/fa";
 
-const Navbar = () => {
+const Navbar = ({ user, logoutUser }) => {
+  const [showLogout, setShowLogout] = useState(false);
+
   return (
     <div className="navigation">
       <nav>
@@ -22,10 +26,21 @@ const Navbar = () => {
             <Hyperlink to="bookings">Bookings</Hyperlink>
           </li>
         </ul>
-
-        <div className="user">
-          <img src={defaultUser} alt="" />
-          <span>Dwayne</span>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => setShowLogout(!showLogout)}
+          >
+            <FaUserCircle />
+            {user?.name || "Seb"}
+            <FaCaretDown />
+          </button>
+          <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
+            <button type="button" className="dropdown-btn" onClick={logoutUser}>
+              Logout
+            </button>
+          </div>
         </div>
       </nav>
     </div>
