0084dcd90d00c936fceedff042d32a591c7e6f02
Dorothy <dorothychauu@gmail.com>
2023-03-08 21:00:27 -0500

    login/signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::API
   SECRET_KEY ='s@ds$b@ur@d0rit0'
 
   def authenticate
-    decoded_token = JWT.decode(request.headers['Authorization'], APP_SECRET, true, { algorithm: 'HS256' }) # take from: https://github.com/jwt/ruby-jwt
+    # byebug
+    decoded_token = JWT.decode(request.headers['Authorization'], SECRET_KEY, true, { algorithm: 'HS256' })
     user = User.find(decoded_token[0]['user_id'])
     if user
       @current_user = user
